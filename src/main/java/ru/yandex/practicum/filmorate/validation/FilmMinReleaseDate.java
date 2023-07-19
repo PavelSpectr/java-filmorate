491f23bb4aa7ab4e9a1f0f1f3164cb494960b942
PavelSpectr <titsubishi@yandex.ru>
2023-07-19 10:47:28 +0800

    Develop

--- src/main/java/ru/yandex/practicum/filmorate/validation/FilmMinReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/FilmMinReleaseDate.java
@@ -1,21 +1,22 @@
-package ru.yandex.practicum.filmorate.validator;
+package ru.yandex.practicum.filmorate.validation;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
+@Constraint(validatedBy = FilmMinReleaseDateValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = AfterDateFilmValidator.class)
-public @interface AfterDateFilm {
-    String message() default "Дата релиза должна быть после 28 декабря 1895 года";
+public @interface FilmMinReleaseDate {
+
+    String message() default "Дата релиза должна быть не раньше 28 декабря 1895 года";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
-
-    String value();
 }
