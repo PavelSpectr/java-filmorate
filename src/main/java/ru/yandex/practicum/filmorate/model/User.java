717643e92384cde4ff950b55a0d4ae9b3f12e038
PavelSpectr <120334093+PavelSpectr@users.noreply.github.com>
2023-07-19 10:27:48 +0800

    Filmorate Sprint11

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,34 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
+
 public class User {
     private Long id;
-    @Email
-    private String email;
-    @NotBlank
-    private String login;
-    private String name;
-    @PastOrPresent
-    private LocalDate birthday;
-    private Set<Long> friends = new HashSet<>();
 
-    //Все таки явное объявление конструктора более кофортно, в плане понимания кода)
-    //Иначе приходится проверять все свойства, а делать все через сеттеры не очень удобно)
-    public User(String email, String login, String name, LocalDate birthday) {
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
+    @NotBlank(message = "Пустая электронная почта")
+    @Email(message = "Некорректная почта")
+    private String email;
+
+    @NotBlank(message = "Пустой логин")
+    @Pattern(regexp = "\\S+", message = "В логине не могут находиться пробелы")
+    private String login;
+
+    private String name;
+
+    @NotNull(message = "Пустая дата рождения")
+    @Past(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+
+    private Set<Long> friends = new HashSet<>();
 }
