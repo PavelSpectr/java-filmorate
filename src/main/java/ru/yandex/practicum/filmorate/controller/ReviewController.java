96eabd8b3585c2003e145744a0439fb1a3188330
KurochkinAndrew <120648391+KurochkinAndrew@users.noreply.github.com>
2023-07-26 21:04:50 +1000

    add-reviews (Andrew): Добавить отзывы
    add-reviews (Andrew): Добавить отзывы

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.ReviewService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @GetMapping("/{reviewId}")
+    public Review getReviewById(@PathVariable Long reviewId) {
+        return reviewService.getReviewById(reviewId);
+    }
+
+    @PostMapping
+    public Review addReview(@Valid @RequestBody Review review) {
+        return reviewService.addReview(review);
+    }
+
+    @PutMapping
+    public Review updateReview(@Valid @RequestBody Review review) {
+        return reviewService.updateReview(review);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public void deleteReview(@PathVariable Long reviewId) {
+        reviewService.deleteReview(reviewId);
+    }
+
+    @GetMapping
+    public List<Review> getReviews(@RequestParam(defaultValue = "", required = false) Long filmId,
+                                   @RequestParam(defaultValue = "10") Long count) {
+        return reviewService.getReviewsOfFilm(filmId, count);
+    }
+
+    @PutMapping("/{reviewId}/like/{userId}")
+    public void addLike(@PathVariable Long reviewId, @PathVariable Long userId) {
+        reviewService.addLike(reviewId, userId);
+    }
+
+    @PutMapping("/{reviewId}/dislike/{userId}")
+    public void addDislike(@PathVariable Long reviewId, @PathVariable Long userId) {
+        reviewService.addDislike(reviewId, userId);
+    }
+
+    @DeleteMapping("/{reviewId}/like/{userId}")
+    public void removeLike(Long reviewId, Long userId) {
+        reviewService.removeLike(reviewId, userId);
+    }
+
+    @DeleteMapping("/{reviewId}/dislike/{userId}")
+    public void removeDislike(Long reviewId, Long userId) {
+        reviewService.removeDislike(reviewId, userId);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -0,0 +1,93 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.ReviewStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class ReviewService {
+    private final ReviewStorage reviewStorage;
+
+    private final FilmStorage filmStorage;
+
+    private final UserStorage userStorage;
+
+    public ReviewService(ReviewStorage reviewStorage, @Qualifier("filmDbStorage") FilmStorage filmStorage,
+                         @Qualifier("userDbStorage") UserStorage userStorage) {
+        this.userStorage = userStorage;
+        this.filmStorage = filmStorage;
+        this.reviewStorage = reviewStorage;
+    }
+
+    public Review getReviewById(Long reviewId) {
+        log.debug("+ getReviewById: reviewId={}", reviewId);
+        Review review = reviewStorage.getReviewById(reviewId);
+        log.debug("- getReviewById: {}", review);
+        return review;
+    }
+
+    public Review addReview(Review review) {
+        log.debug("+ addReview: {}", review);
+        filmStorage.getFilmById(review.getFilmId());
+        userStorage.getUserById(review.getUserId());
+        Review addedReview = reviewStorage.addReview(review);
+        log.debug("- addReview: {}", addedReview);
+        return addedReview;
+    }
+
+    public Review updateReview(Review review) {
+        log.debug("+ updateReview: {}", review);
+        Review updatedReview = reviewStorage.updateReview(review);
+        log.debug("- addReview: {}", updatedReview);
+        return updatedReview;
+    }
+
+    public void deleteReview(Long reviewId) {
+        log.debug("+ deleteReview: reviewId={}", reviewId);
+        reviewStorage.deleteReview(reviewId);
+        log.debug("+ deleteReview: reviewId={}", reviewId);
+    }
+
+    public List<Review> getReviewsOfFilm(Long filmId, Long count) {
+        List<Review> reviews;
+        log.debug("+ getReviewsOfFilm: filmId={}, count={}", filmId, count);
+        if (filmId == null) {
+            reviews = reviewStorage.getReviews(count);
+        } else {
+            reviews = reviewStorage.getReviewsOfFilm(filmId, count);
+        }
+        log.debug("+ getReviewsOfFilm: ", reviews);
+        return reviews;
+    }
+
+    public void addLike(Long reviewId, Long userId) {
+        log.debug("+ addLike: reviewId = {}, userId = {}", reviewId, userId);
+        reviewStorage.addLike(reviewId, userId);
+        log.debug("- addLike: reviewId = {}, userId = {}", reviewId, userId);
+    }
+
+    public void addDislike(Long reviewId, Long userId) {
+        log.debug("+ addDislike: reviewId = {}, userId = {}", reviewId, userId);
+        reviewStorage.addDislike(reviewId, userId);
+        log.debug("- addDislike: reviewId = {}, userId = {}", reviewId, userId);
+    }
+
+    public void removeLike(Long reviewId, Long userId) {
+        log.debug("+ removeLike: reviewId = {}, userId = {}", reviewId, userId);
+        reviewStorage.removeLike(reviewId, userId);
+        log.debug("- removeLike: reviewId = {}, userId = {}", reviewId, userId);
+    }
+
+    public void removeDislike(Long reviewId, Long userId) {
+        log.debug("+ removeDislike: reviewId = {}, userId = {}", reviewId, userId);
+        reviewStorage.removeDislike(reviewId, userId);
+        log.debug("- removeDislike: reviewId = {}, userId = {}", reviewId, userId);
+    }
+}
